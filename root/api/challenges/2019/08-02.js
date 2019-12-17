0ee50f98c33c916390c7226f8d75b1bee689912c
Jason Beck <jason.beck@blackboardinsurance.com>
2019-12-17 11:47:53 -0500

    Day 8, Part 2

--- root/api/challenges/2019/08-02.js
+++ root/api/challenges/2019/08-02.js
@@ -1,44 +1,62 @@
 function imageDecoder() {
-  // 123456789012 (3, 2) => [123, 456], [789, 012]
+  // 0: black, 1: white, 2: transparent
   function processInput(input) {
-    const image = input.replace('\n','');
+    const image = input.replace('\n', '');
 
-    // printImage('123456789012', { width: 3, height: 2});
-    // printImage(image, { width: 25, height: 6})
+    // printRawImage('0222112222120000', { width: 2, height: 2 });
+    // let sample = buildDecodedImage('0222112222120000', { width: 2, height: 2 });
+    // printDecodedImage(sample, { width: 2 });
 
-    const targetLayer = findFewestZeros(image, { width: 25, height: 6 });
-    const ones = targetLayer.match(/[1]/g).length;
-    const twos = targetLayer.match(/[2]/g).length;
-    const answer = ones * twos;
-
-    return answer;
+    let decodedImage = buildDecodedImage(image, { width: 25, height: 6 });
+    let result = printDecodedImage(decodedImage, { width: 25 });
+    return result;
   }
 
-  function findFewestZeros(data, { width, height}) {
-    let fewest = -1;
-    let targetLayer = '';
-    const dim = width * height;
-    for (let layer = 0; layer < data.length; layer = layer + dim)
-    {
-      let layerData = data.substr(layer, dim);
-      let zeros = layerData.match(/[0]/g);
-      // console.log(`Zeros found: ${zeros.length} in ${layerData}`);
-      //console.log(zeros);
-        if (zeros.length < fewest || fewest === -1) {
-          fewest = zeros.length;
-          targetLayer = layerData;
-          console.log(`fewest so far: ${fewest} in ${targetLayer}`);
-        };
+  function printDecodedImage(data, { width }) {
+    // console.log(data);
+    let image = '';
+    const lines = data.length / width;
+    for (let line = 0; line < lines; line++) {
+      let start = line * width;
+      let view = data.slice(start, start + width);
+      let out = view.join('');
+      console.log(out);
+      image = image + out + '\n';
     }
-    return targetLayer;
+    return image;
   }
 
-  function printImage(data, { width, height }) {
+  function buildDecodedImage(data, { width, height }) {
+    const dim = width * height;
+    const layers = data.length / dim;
+    const pixels = data.length / layers;
+    // console.log(`Expecting ${layers} layers, ${pixels} pixels from ${data}`);
+    const decoded = [];
+    for (let pixel = 0; pixel < pixels; pixel++) {
+      let visible = 2;
+      let lyr = 0;
+      while (visible === 2 && lyr < layers) {
+        let start = (lyr * dim) + pixel;
+        let point = Number(data.substr(start, 1));
+        // console.log(`Pixel ${pixel} Layer ${lyr} Start ${start} point ${point}`);
+        if (visible === 2 && point < 2) {
+          visible = point;
+        }
+        lyr++;
+      }
+      //decoded.push(visible);
+      let transcode = visible.toString().replace('0', ' ');
+      transcode = transcode.replace('1', 'X');
+      decoded.push(transcode);
+    }
+    return decoded;
+  }
+
+  function printRawImage(data, { width, height }) {
     // each layer is pixels W wide, H height
     const dim = width * height;
     console.log(`Each layer is ${dim} of the digits`);
-    for (let layer = 0; layer < data.length; layer = layer + dim)
-    {
+    for (let layer = 0; layer < data.length; layer = layer + dim) {
       let layerData = data.substr(layer, dim);
       let line = '';
       for (let pixel = 0; pixel < dim; pixel = pixel + width) {
@@ -47,7 +65,7 @@ function imageDecoder() {
       console.log(line);
     }
   }
-  
+
   return {
     processInput
   };
