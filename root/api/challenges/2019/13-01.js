dd729ffc86eef2bbd1dda3e4ba300517be2010bc
Jason Beck <github.com@jcbeck.com>
2019-12-19 20:42:10 -0500

    Day 13 Part 2 w comments for future visualizations...

--- root/api/challenges/2019/13-01.js
+++ root/api/challenges/2019/13-01.js
@@ -29,51 +29,77 @@ function arcadeCabinet() {
     let blockTiles = 0;
     let objects = [];
     let inputStarted = false;
+    let mode = 0;
     let joystickPosition = joystick.neutral;
-    const positions = [0, 0];
+    //const positions = [1, 1, 0, 0, 0, 1, 1, -1, -1, -1, -1, 0, 0];
     let inputs = 0;
+    let score = 0;
     while (!done) {
-      let x = executor.next();
+      if (inputStarted) {
+        joystickPosition = calculateBestJoystick(objects);
+      }
+      let x = executor.next(joystickPosition);
       if (x.value.type === 'INPUT') {
-        //console.log('input');
-        if (inputs >= positions.length) inputs = positions.length - 1;
-
-        joystickPosition = positions[inputs];
-        executor.next(joystickPosition);
+        mode = 1;
         inputStarted = true;
         inputs++;
       } else if (!x.done) {
+        mode = 0;
         let y = executor.next();
         let t = executor.next();
 
         if (x.value.val === -1 && y.value.val === 0) {
-          // score
-          console.log(`New score ${t.value.val}`);
+          score = t.value.val;
         } else {
           blockTiles += t.value.val === 2 ? 1 : 0;
 
           let cx = x.value.val;
           let cy = y.value.val;
           let ct = t.value.val;
-          //if (ct !== undefined && ct >= 0 && ct < 5) {
+
+          let tile = objects.find(o => o.cx === cx && o.cy === cy);
+          if (tile !== undefined) {
+            tile.ct = ct;
+          } else {
             objects.push({ cx, cy, ct });
-          //}
+          }
         }
 
         done = y.done || t.done;
       }
       done = done || x.done;
-      if (!done && inputStarted) {
-        drawScreen(objects);
-      }
+      //if (done) {
+        //let paddle = objects.find(o => o.ct === 3);
+        //let ball = objects.find(o => o.ct === 4);
+        //if (paddle !== undefined && ball != undefined) {
+          // let js = joystickPosition < 0 ? '<o' : joystickPosition > 0 ? 'o>' : 'o';
+          // console.log(`SCORE[${score}] ROUND[${inputs}] ${js}`);
+          // drawScreen(objects);
+        //}
+      //}
     }
+    console.log(`SCORE[${score}] ROUND[${inputs}]`);
+          drawScreen(objects);
     // console.log(`Block tile count: ${blockTiles}`);
     return {
       blockTiles
     };
   }
 
-  const object = [' ', '_', '#', '-', 'o'];
+  function calculateBestJoystick(objects) {
+    let ball = objects.find(o => o.ct === 4);
+    let paddle = objects.find(o => o.ct === 3);
+    if (ball !== undefined && paddle !== undefined) {
+      if (ball.cx < paddle.cx) {
+        return -1;
+      } else if (ball.cx > paddle.cx) {
+        return 1;
+      }
+    }
+    return 0;
+  }
+
+  const object = [' ', '|', '#', '_', 'O'];
   function drawScreen(objects) {
     let xmin = objects.reduce(function (min, obj) { return min > 0 && obj.cx > min ? min : obj.cx; }, 0);
     let xmax = objects.reduce(function (max, obj) { return max > 0 && obj.cx < max ? max : obj.cx; }, 0);
@@ -81,22 +107,22 @@ function arcadeCabinet() {
     let ymax = objects.reduce(function (max, obj) { return max > 0 && obj.cy < max ? max : obj.cy; }, 0);
 
     // only draw after complete rows
-    if (objects.length % ymax === 0) {
+    //if (objects.length % ymax === 0) {
+    for (let y = ymin; y <= ymax; y++) {
+      let line = '';
       for (let x = xmin; x <= xmax; x++) {
-        let line = '';
-        for (let y = ymin; y <= ymax; y++) {
-          let obj = objects.find(o => o.cx === x && o.cy === y);
-          if (obj !== undefined) {
-            try {
-              line += object[obj.ct];
-            } catch {
-              console.log('Error');
-            }
+        let obj = objects.find(o => o.cx === x && o.cy === y);
+        if (obj !== undefined) {
+          try {
+            line += object[obj.ct];
+          } catch {
+            console.log('Error');
           }
         }
-        console.log(line);
       }
+      console.log(line);
     }
+    //}
   }
 
   return {
